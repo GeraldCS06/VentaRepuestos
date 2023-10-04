6a9f99a77f644c47307afbd40b7f6e6f328bc93a
GeraldCS06 <123041127+GeraldCS06@users.noreply.github.com>
2023-10-03 22:51:21 -0600

    Tarea semana 3 parte 1

---
+++ VentaRepuestos/src/main/java/VentaRepuestos/VentaRepuestosApplication.java
@@ -0,0 +1,13 @@
+package VentaRepuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaRepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaRepuestosApplication.class, args);
+	}
+
+}
